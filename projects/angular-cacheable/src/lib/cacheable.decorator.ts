1b321b13b95b4a85d87ab53789c2caf25b4998c7
Evgeny Gushchin <ev.gushchin@yandex.ru>
2020-11-03 05:16:39 -0600

    decorators key can accept a function

--- projects/angular-cacheable/src/lib/cacheable.decorator.ts
+++ projects/angular-cacheable/src/lib/cacheable.decorator.ts
@@ -1,22 +1,29 @@
 import { CacheableService } from './cacheable.service';
 
-export function Cacheable(config: { key: string; ttl?: number; }) {
-  // tslint:disable-next-line: no-bitwise
-  const hashCode = (s: string) => s.split('').reduce((a: number, b: string) => { a = ((a << 5) - a) + b.charCodeAt(0); return a & a; }, 0);
+export type KeyMakerFunction = (args: any[]) => string;
 
+// tslint:disable-next-line: no-bitwise
+const hashCode = (s: string) => s.split('').reduce((a: number, b: string) => { a = ((a << 5) - a) + b.charCodeAt(0); return a & a; }, 0);
+
+export function Cacheable(config: { key?: string | KeyMakerFunction; ttl?: number; }) {
   // tslint:disable-next-line: space-before-function-paren only-arrow-functions
   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const oldFunc = descriptor.value;
     // tslint:disable-next-line: space-before-function-paren only-arrow-functions
     descriptor.value = function () {
-      const args = [];
-      // tslint:disable-next-line: prefer-for-of
-      for (let i = 0; i < arguments.length; i++) {
-        args.push(arguments[i]);
+      const args = Array.from(arguments);
+
+      let hashKey = '';
+      if (config.key === undefined || config.key === null || (typeof config.key !== 'string' && typeof config.key !== 'function')) {
+        hashKey = propertyKey + '-' + hashCode(JSON.stringify(args));
+      } else if (typeof config.key === 'string') {
+        hashKey = config.key.toString();
+      } else {
+        hashKey = (config.key as KeyMakerFunction)(args);
       }
 
       return CacheableService.getInstance()
-        .get(config.key + '-' + hashCode(JSON.stringify(args)), oldFunc.apply(this, arguments), config.ttl);
+        .get(hashKey, oldFunc.apply(this, arguments), config.ttl);
     };
   };
 }
